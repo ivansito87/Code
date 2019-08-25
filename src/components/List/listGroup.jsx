cf3211443f3ddd7238b1423a7e0d0d4978b76050
Ivan Rendon <ivansito87@gmail.com>
2019-08-25 12:52:08 -0400

    pagination and list group of genres working commit before refactoring abstraction table

--- src/components/List/listGroup.jsx
+++ src/components/List/listGroup.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { MDBListGroup, MDBListGroupItem, MDBContainer, MDBBtn } from "mdbreact";
+import { MDBListGroup, MDBListGroupItem, MDBContainer } from "mdbreact";
 import "./style.css";
 
 const MyListGroup = props => {
+
+  const {items, textProperty, valueProperty, onItemSelect, selectedItem} = props;
+
   return (
     <MDBContainer>
-      <MDBListGroup className="mx-auto">
-        <MDBListGroupItem color="warning" href="" className="cloudy-knoxville-gradient" hover>All Genres</MDBListGroupItem>
+      <MDBListGroup className="mx-auto z-depth-1-half active">
+        {items.map(item => <MDBListGroupItem {...selectedItem === item && {active: true}}
+          key={item[valueProperty]}
+          color="info"
+          onClick={() => onItemSelect(item)}
+          className=""
+          hover
+        >
+          {item[textProperty]}
+        </MDBListGroupItem>)}
+        {/*<MDBListGroupItem color="warning" href="" className="cloudy-knoxville-gradient text-secondary" hover>All Genres</MDBListGroupItem>
         <MDBListGroupItem color="light" href="" className="" hover>Action</MDBListGroupItem>
         <MDBListGroupItem color="light" href="" hover>Comedy</MDBListGroupItem>
-        <MDBListGroupItem color="light" href="" hover>Thriller</MDBListGroupItem>
+        <MDBListGroupItem color="light" href="" hover>Thriller</MDBListGroupItem>*/}
       </MDBListGroup>
     </MDBContainer>
 
   );
+};
+
+MyListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id"
 };
 
 export default MyListGroup;
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -17,18 +17,27 @@ import Like from "./buttons";
 import PaginationPage from "./paginationPage";
 import { paginate } from "../utils/pajinate";
 import MyListGroup from "../components/List/listGroup";
+import { getGenres } from "../services/fakeGenreService";
 
 class TablePage extends Component {
 
   state = {
-    movies: movies,
+    movies: [],
     modal14: false,
     movie_count: movies.length,
     pageSize: 4,
-    currentPage: 1
+    currentPage: 1,
+    genres: getGenres()
   };
 
+  // we will initialize the genres array from when the component mounts
+  // for a full stack app this could be getting a response from the DB
   componentDidMount() {
+
+    const genres = [{name: "All Genres"}, ...getGenres() ];
+
+    this.setState( { movies , genres });
+
     // console.log("movies", this.state.movies);
   }
 
@@ -67,31 +76,50 @@ class TablePage extends Component {
 
   };
 
+  handleGenreSelect = (item) => {
+
+    this.setState({selectedGenre: item, currentPage:1});
+    console.log("Selecting Genre", item);
+
+  };
+
+
   render() {
-    const { pageSize, currentPage, movie_count, movies: allMovies } = this.state;
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const { pageSize, currentPage, movie_count, selectedGenre, movies: allMovies } = this.state;
+
+    const filtered = selectedGenre && selectedGenre._id
+      ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+      : allMovies;
+
+    const movies = paginate(filtered, currentPage, pageSize);
+
     return (
       <MDBRow>
         <MDBCol md="3">
-          <MyListGroup />
+          <MyListGroup
+            items={this.state.genres}
+            selectedItem={this.state.selectedGenre}
+            onItemSelect={this.handleGenreSelect}/>
         </MDBCol>
         <MDBCol md="9">
-      <MDBCard narrow>
+      <MDBCard narrow className="z-depth-1-half">
         <MDBCardHeader
-          className="view view-cascade gradient-card-header purple-gradient d-flex justify-content-between align-items-center py-2 mx-4 mb-3 clearfix">
+          className="view view-cascade gradient-card-header aqua-gradient d-flex justify-content-between align-items-center py-2 mx-4 mb-3 clearfix">
           <div>
             <PaginationPage
-              itemsCount={this.state.movies.length}
+              itemsCount={filtered.length}
               pageSize={pageSize}
               onPageChange={this.handlePageChange}
               currentPage={currentPage}
+              textProperty="name"
+              valueProperty="_id"
             />
             {/*<MDBBtn className="cloudy-knoxville-gradient" color="warning-color-darker" size="sm">All Genres</MDBBtn>
             <MDBBtn className="deep-blue-gradient" color="success-color-darker" size="sm">Action</MDBBtn>
             <MDBBtn className="dusty-grass-gradient" color="warning-color-darker" size="sm">Comedy</MDBBtn>
             <MDBBtn className="tempting-azure-gradient" color="success-color-darker" size="sm">Thriller</MDBBtn>*/}
           </div>
-          <a href={" "} className="h5 font-weight-bold black-text my-4">There are {movie_count} movies in the
+          <a href={" "} className="h5 font-weight-bold black-text my-4">There are {filtered.length} movies in the
             database</a>
         </MDBCardHeader>
         <MDBCardBody className="elevation-demo-surface">
