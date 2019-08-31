443a610fe3b67bd35433f0da4ae2be654ae2a09b
Ivan Rendon <ivansito87@gmail.com>
2019-08-30 23:40:24 -0400

    adds validation to form password and username using Joi library

--- src/components/LoginForm/loginForm.jsx
+++ src/components/LoginForm/loginForm.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Joi from "joi-browser";
 import {
   MDBContainer,
   MDBRow,
   MDBCol,
   MDBCard,
   MDBCardBody,
-  MDBModalFooter,
   MDBIcon,
   MDBCardHeader,
   MDBBtn,
@@ -15,41 +15,67 @@ import Input from "../common/input";
 class LoginForm extends Component {
 
   state = {
-    username: "", // comes from the form input
-    password: "",
+    account: { username: "", password: "" },
+    /* username: "", // comes from the form input
+     password: "",*/
     errors: {}
 
   };
+  // Using lbrary Joi for validation in forms check it out here https://www.npmjs.com/package/@hapi/joi
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
 
   validate = () => {
+    const options = { abortEarly: false };
+    // using joi to validate the inputs from the user
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
     const errors = {};
-    const { username, password } = this.state;
-    if (username.trim() === "")
-      errors.username = "Username is required";
-    if (password.trim() === "")
-      errors.password = "Password is required";
-    this.setState({ errors });
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details)
+      errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     const errors = this.validate();
     console.log(errors);
-    this.setState({ errors });
-    if (errors) return;
+    this.setState({ errors: errors || {} });
   };
 
-  handleChange = (e) => {
-    const username = e.target.value;
-    const password = e.target.value;
-    this.setState({ username, password }, () => {
-      console.log(this.state.username);
-      console.log(this.state.password);
-    });
+  // this validation is for both the user and or / password
+  // and the form as a whole
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+
+    // This is used to validate the form as it changes
+    // we want to give immediate feedback to our user
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account, errors });
   };
 
   render() {
+    const { errors } = this.state;
+
     return (
       <MDBContainer>
         <MDBRow>
@@ -58,7 +84,7 @@ class LoginForm extends Component {
               <MDBCardBody>
                 <MDBCardHeader className="form-header deep-blue-gradient rounded">
                   <h3 className="my-3">
-                    <MDBIcon icon="lock"/> Login: {this.state.username}
+                    <MDBIcon icon="lock"/> Hello: {this.state.account.username}
                   </h3>
                 </MDBCardHeader>
                 <form onSubmit={(e) => this.handleSubmit(e)}>
@@ -69,6 +95,7 @@ class LoginForm extends Component {
                     name={"username"}
                     autofocus={true}
                     handleChange={this.handleChange}
+                    error={errors.username}
                   />
                   <Input
                     label={"Type your password"}
@@ -76,9 +103,11 @@ class LoginForm extends Component {
                     type={"password"}
                     name={"password"}
                     handleChange={this.handleChange}
+                    error={errors.password}
                   />
                   <div className="text-center mt-4">
                     <MDBBtn
+                      disabled={this.validate()}
                       color="light-blue"
                       className="mb-3"
                       type="submit"
@@ -87,12 +116,6 @@ class LoginForm extends Component {
                     </MDBBtn>
                   </div>
                 </form>
-                <MDBModalFooter>
-                  <div className="font-weight-light">
-                    <p>Not a member? Sign Up</p>
-                    <p>Forgot Password?</p>
-                  </div>
-                </MDBModalFooter>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { MDBInput } from "mdbreact";
+import { MDBInput, MDBAlert } from "mdbreact";
 
-const Input = ({ handleChange, name, label, icon, type, autofocus }) => (
+const Input = ({ handleChange, name, label, icon, type, autofocus, error }) => (
   <div className="grey-text">
     <MDBInput
       autoFocus={autofocus} // this is used to auto focus the cursor when the component is render
@@ -15,6 +15,9 @@ const Input = ({ handleChange, name, label, icon, type, autofocus }) => (
       onChange={handleChange}
       name={name}
     />
+    {error && <p >
+      <p className="text-danger">{ error }</p>
+    </p>}
   </div>
 );
 
