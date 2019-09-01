67ee0f941f33a1d99809ed803f917f0a8ec65c92
Ivan Rendon <ivansito87@gmail.com>
2019-08-31 23:18:30 -0400

    building reusable form class and make LoginForm extend form class

--- src/components/LoginForm/loginForm.jsx
+++ src/components/LoginForm/loginForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Joi from "joi-browser";
 import {
   MDBContainer,
@@ -11,8 +11,9 @@ import {
   MDBBtn,
 } from "mdbreact";
 import Input from "../common/input";
+import Form from "../common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
 
   state = {
     data: { username: "", password: "" },
@@ -31,46 +32,9 @@ class LoginForm extends Component {
       .label("Password")
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    // using joi to validate the inputs from the user
-    const { error } = Joi.validate(this.state.data, this.schema, options);
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details)
-      errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors: errors || {} });
-  };
-
-  // this validation is for both the user and or / password
-  // and the form as a whole
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-
-    // This is used to validate the form as it changes
-    // we want to give immediate feedback to our user
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const data = { ...this.state.data };
-    data[input.name] = input.value;
-    this.setState({ data, errors });
+  doSubmit = () => {
+    // do stuff here
+    console.log("Submitted");
   };
 
   render() {
