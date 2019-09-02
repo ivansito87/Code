cdbd50115fed3990160f9c7f230a96561c7655aa
Ivan Rendon <ivansito87@gmail.com>
2019-09-01 23:58:48 -0400

    adds function save movie on fake services.js

--- src/components/common/movieAdd.jsx
+++ src/components/common/movieAdd.jsx
@@ -1,8 +1,81 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
 import Form from "./form";
+import Joi from "joi-browser";
+import { getGenres } from "../../services/fakeGenreService";
 
 class MovieForm extends Form {
+  state = {
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: ""
+    },
+    genres: [],
+    errors: {}
+  };
+
+  schema = {
+    _id: Joi.string(),
+    title: Joi.string()
+      .required()
+      .label("title"),
+    genreId: Joi.string()
+      .required()
+      .label("Genre"),
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate")
+  };
+
+
+  componentDidMount() {
+
+    const { history, match } = this.props;
+    const genres = getGenres();
+
+    this.setState({ genres });
+
+    const movieId = match.params.id;
+    if (movieId === "new") return;
+
+    const movie = getMovie(movieId);
+    /*
+    * I have used replace as the method of history because we dont want the user
+    * to hit the back key or be redirected to a page that is empty or has an error
+    * if there is no movie we should display the not-found 404
+    * */
+    if (!movie) return history.replace("not-found");
+
+
+    this.setState({ data: this.mapToViewModel(movie) });
+
+
+  }
+
+  // if issues change to a regular function
+  mapToViewModel = movie => {
+
+    const {_id, title, genre, numberInStock, dailyRentalRate} = movie;
+    return {
+      _id: _id,
+      title: title,
+      genreId: genre._id,
+      numberInStock: numberInStock,
+      dailyRentalRate: dailyRentalRate
+    }
+
+  };
+
+  dosubmit = () => {
+
+    saveMovie(this.state.data);
+    this.props.history.push("/movies");
+
+  };
 
   render() {
     const { history } = this.props;
@@ -10,7 +83,7 @@ class MovieForm extends Form {
       <MDBContainer>
         <MDBRow>
           <MDBCol md="6" className="m-auto mt-4">
-              <p className="h5 text-center mt-5">Add new movie</p>
+            <p className="h5 text-center mt-5">Add new movie</p>
             <form className="mt-5">
               {this.renderInput(1, "title", "Title", "video", "text", true)}
               {this.renderInput(2, "genre", "Genre", "venus-mars", "text")}
